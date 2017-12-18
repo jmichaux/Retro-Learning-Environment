8c31f25ba666498fde873e305de1b97af7a1720e
Jonathan Michaux <jmichaux@ttic.edu>
2017-12-18 16:45:36 -0600

    added SOR3 specific settings

--- src/environment/Settings.cxx
+++ src/environment/Settings.cxx
@@ -336,6 +336,17 @@ void Settings::setDefaultSettings() {
 	stringSettings.emplace(std::make_pair("MK_player2_character", "scorpion"));
 	intSettings.emplace(std::make_pair("MK_opponent_character", 0));
 
+  // Streets Of Rage 3
+  boolSettings.emplace(std::make_pair("SOR3_test", false));
+  intSettings.emplace(std::make_pair("SOR3_start_level", 1));
+  intSettings.emplace(std::make_pair("SOR3_end_level", 1));
+  intSettings.emplace(std::make_pair("SOR3_difficulty", 1));
+  intSettings.emplace(std::make_pair("SOR3_lives", 3));
+  stringSettings.emplace(std::make_pair("SOR3_player_1_character", "skate"));
+  stringSettings.emplace(std::make_pair("SOR3_game_type", "single"));
+ 
+
+
         // Super Mario Kart
         stringSettings.emplace(std::make_pair("SUPER_MARIO_KART_player1_character", "bowser"));
 
